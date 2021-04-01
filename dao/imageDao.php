e813286de259a03f1a335d04854f6e4248bdd24c
77011-76-02 <77011-76-02@exchange.ad.afpanet>
2021-04-01 16:19:35 +0200

    version 3 get()

--- dao/imageDao.php
+++ dao/imageDao.php
@@ -1,6 +1,16 @@
 <?php 
 
+include_once __DIR__.'/../interface/dao.php';
+include_once __DIR__.'/../utils/DBData.php';
+
 class imageDao implements dao {
+
+    private $conn;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
     create(%titre,$image,$idcompte){
         $sql = "INSERT INTO image(titre,image,idcompte) 
 			VALUES ('$titre', '$image', '$idcompte');
@@ -11,11 +21,24 @@ class imageDao implements dao {
 			$result = $con->query($sql);
             return $result;
     }
-    images getAll(){
+    public function getAll(){
         $sql 	= "SELECT * FROM image";
-        $result = $con->query($sql); 
-        return $result;
+        $result = $this->conn->query($sql); 
+        $images = $result->fetchAll();
+        return $images;
     }
+
+    
+
+
+
+
+
+
+
+
+
+
     update($idimage,$titre,$image,$idcompte){
         $sql = "UPDATE image SET titre='$titre', image = '$image' , idcompte='$idcompte' WHERE id='$idimage';
         $con->query($sql);
--- models/image.php
+++ models/image.php
@@ -1,5 +1,91 @@
 <?php 
 
-class image {
+class image 
+{
+private $idimage;
+private $titre;
+private $image;
+private $idcompte;
 
+
+
+/**
+ * Get the value of idimage
+ */ 
+public function getIdimage()
+{
+return $this->idimage;
+}
+
+/**
+ * Set the value of idimage
+ *
+ * @return  self
+ */ 
+public function setIdimage($idimage)
+{
+$this->idimage = $idimage;
+
+return $this;
+}
+
+/**
+ * Get the value of titre
+ */ 
+public function getTitre()
+{
+return $this->titre;
+}
+
+/**
+ * Set the value of titre
+ *
+ * @return  self
+ */ 
+public function setTitre($titre)
+{
+$this->titre = $titre;
+
+return $this;
+}
+
+/**
+ * Get the value of image
+ */ 
+public function getImage()
+{
+return $this->image;
+}
+
+/**
+ * Set the value of image
+ *
+ * @return  self
+ */ 
+public function setImage($image)
+{
+$this->image = $image;
+
+return $this;
+}
+
+/**
+ * Get the value of idcompte
+ */ 
+public function getIdcompte()
+{
+return $this->idcompte;
+}
+
+/**
+ * Set the value of idcompte
+ *
+ * @return  self
+ */ 
+public function setIdcompte($idcompte)
+{
+$this->idcompte = $idcompte;
+
+return $this;
+}
 }
