7eab37f994d92a7d8ac76f60f969c57e55dd43f8
77011-76-02 <77011-76-02@exchange.ad.afpanet>
2021-04-01 14:34:05 +0200

    version 1 imageDao

--- dao/imageDao.php
+++ dao/imageDao.php
@@ -1,5 +1,23 @@
 <?php 
 
 class imageDao implements dao {
+    create(%titre,$image,$idcompte){
+        $sql = "INSERT INTO image(titre,image,idcpmpte) 
+			VALUES ('$titre', '$image', '$idcompte');
+
+        $con->query($sql); 
+    }
 
 }
+
+/* <?php
+
+interface dao {
+    function get();
+    function getAll();
+    function update();
+    function create();
+    function delete();
+}
+
+*/
