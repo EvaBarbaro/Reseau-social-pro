eab162ca1a368bb460892229f01a04ba03b209a5
Malek <tajinemalek@gmail.com>
2021-04-06 10:04:33 +0200

    sql modifs

--- Modelisation+SQL/SocialConnect_mysql.sql
+++ Modelisation+SQL/SocialConnect_mysql.sql
@@ -2,56 +2,56 @@ CREATE DATABASE IF NOT EXISTS `SOCIALCONNECT` DEFAULT CHARACTER SET utf8 COLLATE
 USE `SOCIALCONNECT`;
 
 CREATE TABLE `COMMENTAIRE` (
-  `idcommentaire` BIGINT NOT NULL,
+  `idcommentaire` INTEGER NOT NULL,
   `description` VARCHAR(100),
   `like` INTEGER NOT NULL,
-  `idpublication` BIGINT NOT NULL,
+  `idpublication` INTEGER NOT NULL,
   PRIMARY KEY (`idcommentaire`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `PUBLICATION` (
-  `idpublication` BIGINT NOT NULL,
+  `idpublication` INTEGER NOT NULL,
   `description` VARCHAR(100),
-  `imageurl` VARCHAR(100),
+  `imageurl` VARCHAR(42),
   `like` INTEGER NOT NULL,
   `statut` VARCHAR(42),
-  `idcompte` BIGINT NOT NULL,
+  `idcompte` INTEGER NOT NULL,
   PRIMARY KEY (`idpublication`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `IMAGE` (
-  `idimage` BIGINT NOT NULL,
+  `idimage` INTEGER NOT NULL,
   `titre` VARCHAR(42),
-  `imageurl` VARCHAR(100),
-  `idcompte` BIGINT NOT NULL,
+  `imageurl` VARCHAR(42),
+  `idcompte` INTEGER NOT NULL,
   PRIMARY KEY (`idimage`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `LIKE_COMMENTAIRE` (
-  `idcompte` BIGINT NOT NULL,
-  `idcommentaire` BIGINT NOT NULL,
+  `idcompte` INTEGER NOT NULL,
+  `idcommentaire` INTEGER NOT NULL,
   PRIMARY KEY (`idcompte`, `idcommentaire`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `UTILISATEUR` (
-  `idutilisateur` BIGINT NOT NULL,
+  `idutilisateur` INTEGER NOT NULL,
   `nomutilisateur` VARCHAR(42),
   `motdepasse` VARCHAR(100),
   `mail` VARCHAR(42),
   `role` VARCHAR(42),
   `statut` BOOLEAN,
-  `identreprise` BIGINT NOT NULL,
+  `identreprise` INTEGER NOT NULL,
   PRIMARY KEY (`idutilisateur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `LIKE_PUBLICATION` (
-  `idcompte` BIGINT NOT NULL,
-  `idpublication` BIGINT NOT NULL,
+  `idcompte` INTEGER NOT NULL,
+  `idpublication` INTEGER NOT NULL,
   PRIMARY KEY (`idcompte`, `idpublication`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `COMPTE` (
-  `idcompte` BIGINT NOT NULL,
+  `idcompte` INTEGER NOT NULL,
   `nom` VARCHAR(42),
   `prenom` VARCHAR(42),
   `photo` VARCHAR(42),
@@ -59,27 +59,29 @@ CREATE TABLE `COMPTE` (
   `grade` VARCHAR(42),
   `departement` VARCHAR(42),
   `date_embauche` DATE,
-  `idutilisateur` BIGINT NOT NULL,
+  `idutilisateur` INTEGER NOT NULL,
   PRIMARY KEY (`idcompte`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `ENTREPRISE` (
-  `identreprise` BIGINT NOT NULL,
+  `identreprise` INTEGER NOT NULL,
   `designation` VARCHAR(42),
   `logo` VARCHAR(42),
   `description` VARCHAR(42),
-  `url` VARCHAR(100),
+  `url` VARCHAR(42),
   `statut` BOOLEAN,
   PRIMARY KEY (`identreprise`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `AMIS` (
-  `idcompte` BIGINT NOT NULL,
-  `idcompte_ami` BIGINT NOT NULL,
+  `idcompte` INTEGER NOT NULL,
+  `idcompte_ami` INTEGER NOT NULL,
   PRIMARY KEY (`idcompte`, `idcompte_ami`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `COMMENTAIRE` ADD FOREIGN KEY (`idpublication`) REFERENCES `PUBLICATION` (`idpublication`);
+ALTER TABLE `COMMENTAIRE` ADD `idcompte` INTEGER NOT NULL;
+ALTER TABLE `COMMENTAIRE` ADD FOREIGN KEY (`idcompte`) REFERENCES `COMPTE` (`idcompte`);
 ALTER TABLE `PUBLICATION` ADD FOREIGN KEY (`idcompte`) REFERENCES `COMPTE` (`idcompte`);
 ALTER TABLE `IMAGE` ADD FOREIGN KEY (`idcompte`) REFERENCES `COMPTE` (`idcompte`);
 ALTER TABLE `LIKE_COMMENTAIRE` ADD FOREIGN KEY (`idcommentaire`) REFERENCES `COMMENTAIRE` (`idcommentaire`);
