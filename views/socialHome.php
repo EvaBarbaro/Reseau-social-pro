4841eefe8121e0d780a82e8eef68bb107fc200e1
Malek <tajinemalek@gmail.com>
2021-04-08 12:11:16 +0200

    vue socialHome modif

--- views/socialHome.php
+++ views/socialHome.php
@@ -1,11 +1,71 @@
-<pre>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+  <a class="navbar-brand" href="#">LOGO</a>
+ 
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo pathUrl().'socialHome'?>">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"  href="<?php echo pathUrl().'mesImages'?>">Profil</a>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
 <?php
-
-print_r($viewVars['publicationList']);
-
-echo "<br/>";
-echo " pub1 liked = ".$viewVars['publicationList'][0]['publication_Liked_Par_Utilisateur']."<br/>";
-echo " com 1 pub1 liked = ".$viewVars['publicationList'][0]['commentaires'][0]['commentaire_Liked_Par_Utilisateur'];
-
+foreach($viewVars['publicationList'] as $pub){
 ?>
-</pre>
+<div class="card  mt-3" style="width: 18rem;">
+<div class="card-header">
+<?php
+ echo $pub['comptePublication']['nomutilisateur'];
+?>
+  </div>
+<?php
+if(!empty($pub['comptePublication']['photo'])){
+?>
+  <img src="..." class="card-img-top" alt="Image introuvable">
+  <?php
+}
+  ?>
+  
+  <div class="card-body">
+    <p class="card-text"><?php
+    echo $pub['publicationInfos']['description'];
+?></p>
+  </div>
+  <div class="card-footer">
+    Card footer
+  </div>
+</div>
+<?php
+foreach($pub['commentaires'] as $com){
+?>
+<div class="card" style="width: 18rem;">
+<div class="card-header">
+<?php
+ echo "commentateur ".$com['commentaire_compte']['nomutilisateur'];
+?>
+  </div>
+<?php
+if(!empty($com['commentaire_compte']['photo'])){
+?>
+  <img src="..." class="card-img-top" alt="Image introuvable">
+  <?php
+}
+  ?>
+  
+  <div class="card-body">
+    <p class="card-text"><?php
+    echo $com['commentaireInfo']['description'];
+?></p>
+  </div>
+  <div class="card-footer">
+    Card footer
+  </div>
+</div>
+<?php
+}
+}?>
