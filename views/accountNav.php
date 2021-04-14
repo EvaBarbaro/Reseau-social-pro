a711e0d66cbaefbddde4a9c66ca8397d9c909691
EvaBarbaro <barbaro.eva@outlook.fr>
2021-04-15 00:09:56 +0200

    visit other member page OK

--- views/accountNav.php
+++ views/accountNav.php
@@ -1,19 +1,28 @@
+<?php
+
+if (!empty($viewVars['compte']['idcompte'])) {
+  $idcompte = $viewVars['compte']['idcompte'];
+} else {
+  $idcompte = $_SESSION['idutilisateur'];
+}
+
+?>
 <div>
 <ul class="nav nav-tabs justify-content-center">
   <li class="nav-item">
-    <a class="nav-link post" href="<?= pathUrl().'monCompte/'.$_SESSION['idutilisateur'].'/mesPublications'?>">Mes publications</a>
+    <a class="nav-link post" href="<?= pathUrl().'monCompte/'.$idcompte.'/mesPublications'?>">Mes publications</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link account" href="<?= pathUrl().'monCompte/'.$_SESSION['idutilisateur']?>">Mon Compte</a>
+    <a class="nav-link account" href="<?= pathUrl().'monCompte/'.$idcompte?>">Mon Compte</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link infos" href="<?= pathUrl().'monCompte/'.$_SESSION['idutilisateur'].'/mesInformations'?>">Mes informations</a>
+    <a class="nav-link infos" href="<?= pathUrl().'monCompte/'.$idcompte.'/mesInformations'?>">Mes informations</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link album" href="<?= pathUrl().'monCompte/'.$_SESSION['idutilisateur'].'/mesImages'?>">Mon Album</a>
+    <a class="nav-link album" href="<?= pathUrl().'monCompte/'.$idcompte.'/mesImages'?>">Mon Album</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link password" href="<?= pathUrl().'monCompte/'.$_SESSION['idutilisateur'].'/monMotDePasse'?>">Modifier mon mot de passe</a>
+    <a class="nav-link password" href="<?= pathUrl().'monCompte/'.$idcompte.'/monMotDePasse'?>">Modifier mon mot de passe</a>
   </li>
   <li class="nav-item">
     <a class="nav-link users" href="<?= pathUrl().'monReseau/'.$_SESSION['identreprise'].'/admin'?>">Gérer les utilisateurs</a>
--- views/image.php
+++ views/image.php
@@ -5,18 +5,6 @@
 <div class="d-flex">
 <?php require_once __DIR__ . '/asideProfil.php'; ?>
     <div class="container">
-        
-        <!DOCTYPE html>
-        <html lang="fr">
-            <head>
-                <!--<title>Bootstrap Example</title> -->
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-            </head>
-            <body>
                 <div class="d-flex">
 
                         <div class="container">
