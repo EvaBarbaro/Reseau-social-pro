db9560a0660eea4a591dd56836685a20285882c1
MalekTajine <tajinemalek@gmail.com>
2021-04-19 16:48:14 +0200

    mes publications

--- views/publicationsByUser.php
+++ views/publicationsByUser.php
@@ -20,34 +20,15 @@ require_once __DIR__ . '/asideProfil.php';
                     $publication = $viewVars['publicationByUser'][$index];
             ?>
             <div class="card col-8 mb-4">
-            <?php 
-            if(!empty($publication["imageurl"])){
-            ?>
             <img class="card-img-top" src=<?= pathUrl()."public/publicationImages/".$publication["imageurl"] ?> alt="Card image cap">
-            <?php }  else if(!empty($publication["videourl"])){
-            ?>
-             <video class="card-img-top" width="520" height="440" controls>
-  <source src=<?=pathUrl().'public/publicationVideos/'.$publication['videourl']?> type="video/mp4">
-
-  Your browser does not support the video tag.
-</video> 
-            <?php }  else if(!empty($publication["fichierurl"])){
-            ?>
-             <div>
-    <object class="card-img-top" data="<?=pathUrl().'public/publicationFichiers/'.$publication['fichierurl']?>" type="application/pdf" width="520" height="440">
-
-    </object>
-</div>
-            <?php } ?>  
-            
             <div class="card-body">
                 <?php
                     if ($publication['idcompte'] === $_SESSION['idutilisateur']) {
                 ?>
             <form class="formUpdatePost" action="<?= pathUrl()."mesPublications/update" ?>" method="POST">
-                <img id="<?='updateTitlePost'.$index?>" src="<?= pathUrl()."public/img/pencil.png" ?>" alt="bouton modifier description" width="5%" length="5%" class="imagePostUser float-right">
-                <textarea type="text" name="description" id="<?='descriptionPost'.$index?>" class="fakeTextInput  float-right" readonly><?= $publication["description"]?></textarea>
-                <button type='submit' class='btn btn-info float-none postButton'>Modifier</button>
+                <img id="<?='updateTitlePost'.$index?>" src="<?= pathUrl()."public/img/pencil.png" ?>" alt="bouton modifier description" width="5%" class="imagePostUser">
+                <textarea type="text" name="description" id="<?='descriptionPost'.$index?>" class="fakeTextInput postDescUser" readonly><?= $publication["description"]?></textarea>
+                <button type='submit' class='btn btn-info row float-right postButton'>Modifier</button>
                 <input type="hidden" name="idpublication" value=<?= $publication["idpublication"]?>>
                 <input type="hidden" name="imageurl" value=<?= $publication["imageurl"]?>>
                 <input type="hidden" name="like" value=<?= $publication["like"]?>>
@@ -55,8 +36,7 @@ require_once __DIR__ . '/asideProfil.php';
                 <input type="hidden" name="idcompte" value=<?= $publication["idcompte"]?>>
             </form>
              <!-- Button trigger modal -->
-             <br>
-             <button id="pubModalDel" type="button" class="btn btn-danger float-none  postButton" data-toggle="modal" data-target="<?='#postModal'.$publication["idpublication"] ?>">
+             <button id="pubModalDel" type="button" class="btn btn-danger float-right mt-2 postButton" data-toggle="modal" data-target="<?='#postModal'.$publication["idpublication"] ?>">
                 Supprimer
                 </button>
 
