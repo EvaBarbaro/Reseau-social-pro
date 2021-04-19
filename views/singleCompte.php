f6606f577565f1c7cd6fca2b645ef0a7ca934382
LuisBussy <olivierosulf@gmail.com>
2021-04-19 13:31:57 +0200

    suppression commentaire dans le code

--- views/singleCompte.php
+++ views/singleCompte.php
@@ -69,13 +69,13 @@
                 <button type="submit" class="btn btn-info btn-lg btn-block">Obtenir une attestation de travaille</button>         
             </form>
             <br>
-    <pre>
-        <b><p style="color:red;" >Variable SESSION</p></b>    <?php  var_dump($_SESSION); ?> 
-        <b><p style="color:red;">Variable viewvars</p> </b>    <?php  var_dump($viewVars); ?> 
-        <b><p style="color:red;"> Variable POST</p></b>   <?php  var_dump($_POST); ?> 
-        <b><p style="color:red;">Variable getAll</p></b>   <?php  var_dump($_GET); ?> 
-        <b><p style="color:red;">Variable FILES</p> </b>    <?php  var_dump($_FILES); ?> 
-    </pre>
+    <!-- <pre>
+        <b><p style="color:red;" >Variable SESSION</p></b>    <?php  //var_dump($_SESSION); ?> 
+        <b><p style="color:red;">Variable viewvars</p> </b>    <?php  //var_dump($viewVars); ?> 
+        <b><p style="color:red;"> Variable POST</p></b>   <?php  //var_dump($_POST); ?> 
+        <b><p style="color:red;">Variable getAll</p></b>   <?php // var_dump($_GET); ?> 
+        <b><p style="color:red;">Variable FILES</p> </b>    <?php  //var_dump($_FILES); ?> 
+    </pre> -->
 
         </div>
 </div>
